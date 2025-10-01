c7b7f66c8ee21e5ca87048fdfccfe4040ead5782
PAULO_HENRIQUE_RESENDE_VIANNA <72400820@SIGMAB24.BARROCA>
2025-09-30 21:10:34 -0300

    Alterações no retorno das funções de soma e subtração

--- TesteCalculadora/src/Calculadora.java
+++ TesteCalculadora/src/Calculadora.java
@@ -7,15 +7,13 @@ public class Calculadora {
     }
 
     public int somar(int a, int b) {
-        int resultado = a + b;
         // Interage com o histórico para registrar a operação
-        historico.registrar(a + " + " + b + " = " + resultado);
-        return resultado;
+        historico.registrar(a + " + " + b + " = " + (a + b));
+        return (a + b);
     }
 
     public int subtrair(int a, int b) {
-        int resultado = a - b;
-        historico.registrar(a + " - " + b + " = " + resultado);
-        return resultado;
+        historico.registrar(a + " - " + b + " = " + (a - b));
+        return (a - b);
     }
 }
